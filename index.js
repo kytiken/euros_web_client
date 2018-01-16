736b73b0beb7c6576b91a9361cd6cc649131e1e7
kytiken <kytiken@gmail.com>
2018-01-16 22:55:24 +0900

    WIP: Add channel connection

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+import { Socket } from 'phoenix/assets/js/phoenix';
+let socket = new Socket("ws://localhost:32776/socket", {params: {userToken: "123"}})
+socket.connect()
+let channel = socket.channel("desk:lobby", {token: "hoge"})
+channel.on("new_msg", msg => console.log("Got message", msg) )
+
+channel.join()
+  .receive("ok", ({messages}) => console.log("catching up", messages) )
+  .receive("error", ({reason}) => console.log("failed join", reason) )
+  .receive("timeout", () => console.log("Networking issue. Still waiting..."))
