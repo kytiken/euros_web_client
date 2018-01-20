2f2aeeafe8631155a8f6b52b0aa9adb516d67614
kytiken <kytiken@gmail.com>
2018-01-21 00:02:45 +0900

    Add sample

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import { Socket } from 'phoenix/assets/js/phoenix';
-let socket = new Socket("ws://localhost:32776/socket", {params: {userToken: "123"}})
+let socket = new Socket("ws://localhost:32781/socket", {params: {userToken: "123"}})
 socket.connect()
 let channel = socket.channel("desk:lobby", {token: "hoge"})
-channel.on("new_msg", msg => console.log("Got message", msg) )
+channel.on("crawl", msg => console.log("Got message", msg) )
+setInterval(() => {
+  channel.push("crawl", {url: "https://euros-test.blogspot.jp/"}, 10000)
+    .receive("ok", (msg) => console.log("created message", msg) )
+    .receive("error", (reasons) => console.log("create failed", reasons) )
+    .receive("timeout", () => console.log("Networking issue...") )
+}, 10000)
 
 channel.join()
   .receive("ok", ({messages}) => console.log("catching up", messages) )
