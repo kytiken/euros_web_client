5038292120fa22f94b963f907661c7c0d0292531
kytiken <kytiken@gmail.com>
2018-01-21 10:47:33 +0900

    Add sample

---
+++ components/Page.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'next/link';
+import { connect } from 'react-redux';
+import Clock from './Clock';
+import AddCount from './AddCount';
+
+const Page = ({
+  title,
+  linkTo,
+  lastUpdate,
+  light,
+}) =>
+  (
+    <div>
+      <h1>{title}</h1>
+      <Clock lastUpdate={lastUpdate} light={light} />
+      <AddCount />
+      <nav>
+        <Link href={linkTo}>
+          <span>Navigate</span>
+        </Link>
+      </nav>
+    </div>
+  );
+
+
+Page.propTypes = {
+  title: PropTypes.string.isRequired,
+  linkTo: PropTypes.string.isRequired,
+  lastUpdate: PropTypes.number.isRequired,
+  light: PropTypes.bool.isRequired,
+};
+
+export default connect(state => state)(Page);
