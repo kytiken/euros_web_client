ea76978296abf5888d95954b54587acedcd2d242
kytiken <kytiken@gmail.com>
2018-01-27 13:07:52 +0900

    Add config file for set url (#7)

--- pages/desk.js
+++ pages/desk.js
@@ -4,6 +4,7 @@ import dynamic from 'next/dynamic';
 import withRedux from 'next-redux-wrapper';
 import initStore from '../store';
 import { addDocument, cleanDocuments } from '../actions';
+import config from '../config';
 
 const mapStateToProps = state =>
   ({ documents: state.documents });
@@ -11,7 +12,7 @@ const mapStateToProps = state =>
 const mapDispatchToProps = (dispatch) => {
   let channel = {};
   if (typeof window !== 'undefined') {
-    const socket = new Socket('ws://localhost:32771/socket', { params: { userToken: '123' } });
+    const socket = new Socket(config.socket_url, { params: { userToken: '123' } });
     socket.connect();
     channel = socket.channel('desk:lobby', { token: 'hoge' });
     channel.join();
