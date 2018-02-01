b229608353f143f9da112030c3bc0a0043f6a1af
kytiken <kytiken@gmail.com>
2018-02-01 12:38:25 +0900

    Fix desk show logic

--- components/Desk.js
+++ components/Desk.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
 import Document from './Document';
+import config from '../config';
 
 class Desk extends React.Component {
   static getInitialProps({ isServer }) {
@@ -14,6 +15,10 @@ class Desk extends React.Component {
     /* eslint-disable */
       this.props.addDocument(msg.url)
     });
+    console.log(this.props.url.query.crawlId);
+    fetch(config.crawl_url + '/' + this.props.url.query.crawlId.toString() + '/documents')
+      .then(response => response.json())
+      .then(response => response.data.forEach(doc => this.props.addDocument(doc.url)));
   }
 
   componentWillUnmount() {
