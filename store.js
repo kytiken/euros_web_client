5fb3466c16b660a3aa7931ca1521f7169b7a71ac
kytiken <kytiken@gmail.com>
2018-02-10 16:41:37 +0900

    Add redux-actions (#26)

--- store.js
+++ store.js
@@ -1,13 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { List } from 'immutable';
 import thunkMiddleware from 'redux-thunk';
-import reducer from './reducers';
+import { reducer, defaultState } from './reducers';
 
-const initialStateValue = {
-  documents: List(),
-  crawls: List(),
-};
-
-export default (initialState = initialStateValue) =>
-  createStore(reducer, initialState, composeWithDevTools(applyMiddleware(thunkMiddleware)));
+export default (state = defaultState) =>
+  createStore(reducer, state, composeWithDevTools(applyMiddleware(thunkMiddleware)));
